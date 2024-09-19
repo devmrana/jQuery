9a00ca89c8c8b43b14adc7764b3d79e91ef8e207
mranapy <masudcse74@gmail.com>
2024-09-19 12:30:33 +0600

    working..

---
+++ index.js
@@ -0,0 +1,37 @@
+// test
+// $("p").text("Masud");
+
+// // selector
+// $(document).ready(function () {
+//     $("button").click(function () {
+//         alert(1);
+//     });
+// });
+// // id#, class. , *
+// $("h3,p").text("NO NO");
+// $(".mydiv p").text("NO NO");
+// $(".newdiv p").text("wow working");
+
+// $(".newdiv p").html("<button class='btn btn-danger'>click</button>");
+
+// $(".newdiv p").append(" Never give up...");
+// $(".newdiv p").prepend("Hi, ");
+
+// var mytxt = $("<p></p>").text("before 1");
+// $(".mydiv p").before(mytxt);
+// let mytxt2 = $("<p></p>").text("after 2");
+// $(".newdiv p").after(mytxt2);
+
+// $(".hreftag").attr("href", "https://www.google.com");
+// $(".hreftag").remove("href");
+// $(".hreftag").attr("href");
+$(".hreftag").hide("href");
+// $("p").css("color", "red");
+// $("p").css({
+//     "font-size": "2rem",
+//     "font-style": "italic"
+// });
+
+$(".mydiv p").addClass("style1 commonstyle");
+$(".newdiv p").addClass("style2 commonstyle");
+
